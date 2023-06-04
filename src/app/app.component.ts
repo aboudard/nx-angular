85aa3055ebbc13716dd62da767568071c85ab5b5
Alain Boudard <coco.boudard@gmail.com>
2023-06-04 12:01:02 +0200

    feat: binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FormsModule],
   selector: 'nx-angular-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  bind = 'bind';
   title = 'nx-angular';
 }
